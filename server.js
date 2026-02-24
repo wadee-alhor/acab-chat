ba45808855ba03ff56d72d7649f9f21aad628630
wadee-alhor <wadeealhor8@gmail.com>
2026-02-24 23:37:47 +0200

    update

--- server.js
+++ server.js
@@ -1,52 +1,54 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const User = require("./models/User");
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-let onlineUsers = [];
+mongoose.connect("mongodb://127.0.0.1:27017/chatapp");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashed });
+  await user.save();
+  res.json({ message: "تم إنشاء الحساب" });
 });
 
-io.on('connection', (socket) => {
-  console.log('User connected: ' + socket.id);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ message: "المستخدم غير موجود" });
 
-  socket.on('new user', (username) => {
-    if (!username || onlineUsers.includes(username)) {
-      socket.emit('username taken');
-      return;
-    }
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(400).json({ message: "كلمة المرور خطأ" });
 
-    socket.username = username;
-    onlineUsers.push(username);
-    io.emit('update users', onlineUsers);
+  const token = jwt.sign({ id: user._id }, "SECRET_KEY");
+  res.json({ token, username: user.username });
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
   });
 
-  socket.on('chat message', (msg) => {
-    if (!socket.username) return;
-
-    io.emit('chat message', {
-      user: socket.username,
-      msg: msg
-    });
-  });
-
-  socket.on('disconnect', () => {
-    if (socket.username) {
-      onlineUsers = onlineUsers.filter(u => u !== socket.username);
-      io.emit('update users', onlineUsers);
-    }
-
-    console.log('User disconnected: ' + socket.id);
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
-/* ✅ مهم للاستضافة */
-const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
