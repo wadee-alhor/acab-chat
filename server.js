2ecb0df755ef5f17767f8b33810810a0cd98e601
wadee-alhor <wadeealhor8@gmail.com>
2026-02-25 00:06:48 +0200

    Discord Clone

--- server.js
+++ server.js
@@ -1,55 +1,26 @@
-console.log("WAD NEW VERSION");
 const express = require("express");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
-const User = require("./models/User.js");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(cors());
-app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/chatapp");
-
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const hashed = await bcrypt.hash(password, 10);
-  const user = new User({ username, password: hashed });
-  await user.save();
-  res.json({ message: "تم إنشاء الحساب" });
-});
-
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (!user) return res.status(400).json({ message: "المستخدم غير موجود" });
-
-  const valid = await bcrypt.compare(password, user.password);
-  if (!valid) return res.status(400).json({ message: "كلمة المرور خطأ" });
-
-  const token = jwt.sign({ id: user._id }, "SECRET_KEY");
-  res.json({ token, username: user.username });
-});
-
 io.on("connection", (socket) => {
-  console.log("User connected");
 
-  socket.on("message", (msg) => {
-    io.emit("message", msg);
-  });
+    socket.on("join-room", (room, username) => {
+        socket.join(room);
+        socket.to(room).emit("message", username + " joined the room");
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+    socket.on("send-message", (data) => {
+        io.to(data.room).emit("message", data.username + ": " + data.message);
+    });
+
+    socket.on("voice-signal", (data) => {
+        socket.to(data.room).emit("voice-signal", data.signal);
+    });
+
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log("Server running on port " + PORT));
